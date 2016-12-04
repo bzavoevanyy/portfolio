c9348d0ea8d694f4c2f426e2d581c6250c214fa0
Bogdan Zavoevanyy <bzavoevanyy@gmail.com>
2016-12-04 09:50:56 +0500

    for gh-pages

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  setTimeout(function () {
+    document.querySelector('.greating_picture').classList.add('m--show');
+  }, 1000);
+})();
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbiAoKSB7XG4gICd1c2Ugc3RyaWN0JztcblxuICBzZXRUaW1lb3V0KGZ1bmN0aW9uICgpIHtcbiAgICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcuZ3JlYXRpbmdfcGljdHVyZScpLmNsYXNzTGlzdC5hZGQoJ20tLXNob3cnKTtcbiAgfSwgMTAwMCk7XG59KSgpO1xuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
